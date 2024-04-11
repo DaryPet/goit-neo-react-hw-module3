305942742badd4c4772d6ae2162d655bf7c590f9
DaryPet <d7akkord@gmail.com>
2024-04-11 15:16:57 +0200

    hw_3_all jsx

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -1,30 +1,30 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import * as Yup from "yup";
+// import * as Yup from "yup";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
 import InitialContacts from "../contacts.json";
+import { nanoid } from "nanoid";
 
 export default function App() {
-  const [contacts, setContacts] = useState(InitialContacts);
+  const [contacts, setContacts] = useState((InitialContacts) => {
+    const savedContact = window.localStorage.getItem("saved-contacts");
+    if (savedContact !== null) {
+      return JSON.parse(savedContact);
+    }
+    return contacts;
+  });
+
   const [filter, setFilter] = useState("");
 
   const visibleContact = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
-  // const ContactSchema = Yup.object().shape({
-  //   name: Yup.string()
-  //     .min(3, "Too short!")
-  //     .max(50, "Too long!")
-  //     .required("Required"),
-  //   tel: Yup.string().phone("UA", true).required("Required"),
-  // });
-
   const addContact = (newContact) => {
     setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
+      return [...prevContacts, { ...newContact, id: nanoid() }];
     });
   };
 
@@ -34,14 +34,14 @@ export default function App() {
     });
   };
 
+  useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm
-        initial={InitialContacts}
-        // validation={ContactSchema}
-        onAdd={addContact}
-      />
+      <ContactForm initial={InitialContacts} onAdd={addContact} />
       <SearchBox value={filter} onFilter={setFilter} />
       <ContactList contacts={visibleContact} onDelete={delContact} />
     </div>
--- vite-project/src/components/Contacts/Contacts.jsx
+++ vite-project/src/components/Contacts/Contacts.jsx
@@ -1,11 +1,18 @@
 import css from "./Contacts.module.css";
+import { RiUser3Fill } from "react-icons/ri";
+import { BiSolidPhone } from "react-icons/bi";
 
 export default function Contacts({ data: { id, name, number }, onDelete }) {
   return (
     <div className={css.container}>
       <div className={css.data}>
-        <p className={css.text}>{name}</p>
-        <p className={css.text}>{number}</p>
+        <p className={css.text}>
+          <RiUser3Fill /> {name}
+        </p>
+        <p className={css.text}>
+          <BiSolidPhone />
+          {number}
+        </p>
       </div>
 
       <button className={css.btn} onClick={() => onDelete(id)}>
